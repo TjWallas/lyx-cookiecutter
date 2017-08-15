3bd0ee0cc13afa502df5449c18acafb7cac9d77d
TjWallas <tjwallas@cryptolab.net>
2017-08-15 18:55:49 +0200

    Symbolically link and activate git hooks when project is initialized

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/usr/bin/git init
+/usr/bin/git init && \
+/bin/ln -s -f ../../.git_hooks/post-commit .git/hooks/post-commit && \
+/bin/ln -s -f ../../.git_hooks/pre-commit .git/hooks/pre-commit
